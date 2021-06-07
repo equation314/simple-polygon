85e85da731159a019e5fd12377da532571734ad4
许大仙 <xuyiwen14@gmail.com>
2021-06-07 22:39:16 +0800

    Update web front-end
    modify index.html and index.js

--- web/site/index.js
+++ web/site/index.js
@@ -5,10 +5,15 @@ import { Draw, getRandomColor } from "./draw.js";
 var draw = new Draw();
 var points = [];
 
+function drawPolygon() {
+    draw.removeAllShapes();
+}
+
 function randomPolygon() {
     points = SP.gen_polygon();
     console.log(points);
-    draw.drawPolygon(points, { color: getRandomColor(), vertexSize: 3, class: "fixed" });
+    draw.removeAllShapes();
+    draw.drawPolygon(points, { color: getRandomColor(), vertexSize: 3 });
     draw.autoScale(points);
     $("#path-btn").removeClass("disabled");
 }
@@ -20,6 +25,9 @@ $(() => {
         $("#move-btn").addClass("active");
         $("#polygon-btn").removeClass("active");
         $("#path-btn").removeClass("active");
+        $("#right-opts").children().each(function(){
+            $(this).addClass("visually-hidden");
+        });
         draw.setMode("move");
     });
     document.getElementById("polygon-btn").addEventListener("hidden.bs.dropdown", () => {
@@ -30,6 +38,7 @@ $(() => {
         switch ($("#polygon-btn").attr("mode")) {
             case "draw":
                 draw.setMode("draw");
+                drawPolygon();
                 break;
             case "random":
                 draw.setMode("fixed");
@@ -45,7 +54,6 @@ $(() => {
         $("#move-btn").removeClass("active");
         $("#polygon-btn").removeClass("active");
         $("#path-btn").addClass("active");
-
         let diagonals = SP.triangulation(points, "mono_partition");
         console.log(diagonals);
         draw.drawLines(diagonals.map(d => [points[d[0]], points[d[1]]]));
@@ -55,11 +63,25 @@ $(() => {
     });
     $("#draw-btn").on("click", () => {
         $("#polygon-btn").attr("mode", "draw").text("Draw");
+        $("#right-opts li").eq(0).removeClass("visually-hidden");
+        $("#right-opts li").eq(1).removeClass("visually-hidden");
+        for( var i = 2; i < 5; i++){
+            $("#right-opts li").eq(i).addClass("visually-hidden");
+        }
     });
     $("#random-btn").on("click", () => {
         $("#polygon-btn").attr("mode", "random").text("Random");
+        $("#right-opts").children().each(function(){
+            $(this).removeClass("visually-hidden");
+        });
     });
     $("#load-btn").on("click", () => {
         $("#polygon-btn").attr("mode", "load").text("Load from...");
+    });
+    $("#clear-btn").on("click",() =>{
+        draw.removeAllShapes();
+        $("#right-opts").children().each(function(){
+            $(this).addClass("visually-hidden");
+        });
     });
 });
